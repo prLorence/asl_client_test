5dab54d166137707bed5cf7a6e2cdc5674f637f3
prLorence <paulorencereyes@gmail.com>
2023-04-28 15:16:52 +0800

    feat: finished test client app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        // https://ik.imagekit.io/335jblpnu5/profile.png
+        port: '',
+        pathname: '/335jblpnu5/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
